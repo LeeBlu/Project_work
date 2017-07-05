0a859d3a2501a7deeee177b744699a457c3e7b05
Lepelle Seshiba <LSeshiba@jhb.dvt.co.za>
2017-07-05 09:50:14 +0200

    address ops

---
+++ DataBase_CodeFirst/DataBase_CodeFirst/Operations/AddressOps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer_2.ContextFolder;
+using DataLayer_2.Model;
+
+namespace DataBase_CodeFirst.Operations
+{
+    
+   public class AddressOps
+    {
+    
+
+        public void InsertAddress(Address addres,int id,int addtype)
+        {
+            using (var db = new TableContext())
+            {
+                if ()
+                {
+                    var type = db.addresstype.Find(addtype);
+                    addres.Addresstype = type;
+                    db.addresse.Add(addres);
+                    db.SaveChanges();
+                    addres.a
+                }
+                
+            }
+
+        }
+
+    }
+}
